1071ea6ab31bb00c27cec6fa8d7e828b8f36d6d7
CuongD <daccuongdinh9703@gmail.com>
2020-12-18 00:39:05 +0700

    make, cmake ok

---
+++ CMakeFiles/tcp_client.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cuongd/CuongD/VHT_Task/tcp_client/source/client_utils.cpp" "/home/cuongd/CuongD/VHT_Task/tcp_client/CMakeFiles/tcp_client.dir/source/client_utils.cpp.o"
+  "/home/cuongd/CuongD/VHT_Task/tcp_client/source/communication.cpp" "/home/cuongd/CuongD/VHT_Task/tcp_client/CMakeFiles/tcp_client.dir/source/communication.cpp.o"
+  "/home/cuongd/CuongD/VHT_Task/tcp_client/source/tcp_client.cpp" "/home/cuongd/CuongD/VHT_Task/tcp_client/CMakeFiles/tcp_client.dir/source/tcp_client.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "./header"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/tcp_client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_client.dir/source/communication.cpp.o"
+  "CMakeFiles/tcp_client.dir/source/client_utils.cpp.o"
+  "CMakeFiles/tcp_client.dir/source/tcp_client.cpp.o"
+  "tcp_client.pdb"
+  "tcp_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcp_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
